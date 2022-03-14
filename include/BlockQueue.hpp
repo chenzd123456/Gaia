09e7465173264a1ea340a1f92012fff1772c3af9
chenzd123456 <chenzd123456@gmail.com>
2022-03-14 14:06:07 +0800

    add BlockQueue

---
+++ include/BlockQueue.hpp
@@ -0,0 +1,60 @@
+#ifndef __GAIA_BLOCKQUEUE_H__
+#define __GAIA_BLOCKQUEUE_H__
+
+#include <stdint.h>
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace gaia {
+
+template <typename T>
+class BlockQueue {
+ private:
+  uint32_t m_max_size;
+  std::condition_variable m_not_empty_cv;
+  std::condition_variable m_not_full_cv;
+  std::mutex m_mutex;
+  std::queue m_queue;
+
+ public:
+  BlockQueue() : m_max_size(0) {}
+
+  void Push(T item) {
+    std::unique_lock<std::mutex> lock(mutex);
+    const auto is_full = [&] {
+      return m_max_size != 0 && m_max_size >= m_queue.size();
+    };
+    m_not_full_cv.wait(lock, is_full);
+    m_queue.push(item);
+    lock.unlock();
+    m_not_empty_cv.notify_one();
+  }
+
+  T Pop() {
+    std::unique_lock<std::mutex> lock(mutex);
+    const auto is_empty = [&] { return m_queue.empty(); };
+    m_not_empty_cv.wait(lock, is_empty);
+    auto item = m_queue.front();
+    m_queue.pop();
+    lock.unlock();
+    m_not_full_cv.notify_one();
+    return item;
+  }
+
+  uint32_t Size() {
+    std::lock_guard<std::mutex> lock(mutex);
+    return m_queue.size();
+  }
+
+  uint32_t MaxSize() {
+    std::lock_guard<std::mutex> lock(mutex);
+    return m_max_size;
+  }
+
+  void MaxSize(uint32_t max_size) { m_max_size(max_size); }
+};
+}  // namespace gaia
+
+#endif  // __GAIA_BLOCKQUEUE_H__
